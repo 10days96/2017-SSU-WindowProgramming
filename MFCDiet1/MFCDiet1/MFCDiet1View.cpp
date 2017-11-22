451185d41daec3b7dd31bf03046d5cf5994d3258
changbambi <doradoralee@naver.com>
2017-11-22 19:10:20 +0900

    세로

---
+++ MFCDiet1/MFCDiet1/MFCDiet1View.cpp
@@ -0,0 +1,83 @@
+
+// MFCDiet1View.cpp : CMFCDiet1View 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFCDiet1.h"
+#endif
+
+#include "MFCDiet1Doc.h"
+#include "MFCDiet1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCDiet1View
+
+IMPLEMENT_DYNCREATE(CMFCDiet1View, CFormView)
+
+BEGIN_MESSAGE_MAP(CMFCDiet1View, CFormView)
+END_MESSAGE_MAP()
+
+// CMFCDiet1View 생성/소멸
+
+CMFCDiet1View::CMFCDiet1View()
+	: CFormView(IDD_MFCDIET1_FORM)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCDiet1View::~CMFCDiet1View()
+{
+}
+
+void CMFCDiet1View::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CMFCDiet1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CMFCDiet1View::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+
+// CMFCDiet1View 진단
+
+#ifdef _DEBUG
+void CMFCDiet1View::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CMFCDiet1View::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CMFCDiet1Doc* CMFCDiet1View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCDiet1Doc)));
+	return (CMFCDiet1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCDiet1View 메시지 처리기
---
+++ MFCDiet1/MFCDiet1/MFCDiet1View.h
@@ -0,0 +1,52 @@
+
+// MFCDiet1View.h : CMFCDiet1View 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CMFCDiet1View : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CMFCDiet1View();
+	DECLARE_DYNCREATE(CMFCDiet1View)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_MFCDIET1_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CMFCDiet1Doc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+
+// 구현입니다.
+public:
+	virtual ~CMFCDiet1View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFCDiet1View.cpp의 디버그 버전
+inline CMFCDiet1Doc* CMFCDiet1View::GetDocument() const
+   { return reinterpret_cast<CMFCDiet1Doc*>(m_pDocument); }
+#endif
+
