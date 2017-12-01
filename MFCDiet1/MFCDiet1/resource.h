03e8a882e4b32e0218efddd3f826a5aaa26e45e5
choco5226 <choco5226@naver.com>
2017-12-01 18:49:30 +0900

    dnddppdr

---
+++ MFCDiet1/MFCDiet1/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCDiet1.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDD_MFCDIET1_FORM               101
+#define IDR_MAINFRAME                   128
+#define IDR_MFCDiet1TYPE                130
+#define IDD_DIALOG1                     319
+#define IDD_DIALOG2                     320
+#define IDD_DIALOG3                     321
+#define IDD_DIALOG4                     322
+#define IDB_BITMAP1                     329
+#define IDC_MONTHCALENDAR1              1000
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_BUTTON1                     1004
+#define IDC_BUTTON2                     1005
+#define IDC_BUTTON3                     1006
+#define IDC_TAB1                        1007
+#define IDC_BUTTON6                     1010
+#define IDC_EDIT4                       1011
+#define IDC_BUTTON7                     1012
+#define IDC_BUTTON8                     1013
+#define IDC_Kcal                        1015
+#define IDC_BUTTON9                     1016
+#define IDC_MFCEDITBROWSE1              1018
+#define IDC_LIST1                       1019
+#define IDC_LIST2                       1020
+#define IDC_LIST3                       1021
+#define IDC_LIST4                       1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        330
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
